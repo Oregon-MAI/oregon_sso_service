61e11aba2696231bd00a68367beef94fe7421863
Leonid Gurianov <leonid.11521@gmail.com>
2026-03-19 00:37:55 +0300

    add Dto's

--- src/app/data/schemas/User.py
+++ src/app/data/schemas/User.py
@@ -1,8 +1,23 @@
 from pydantic import BaseModel, EmailStr
 
-class UserDto(BaseModel):
+from app.data.schemas.Role import RoleDto
+
+class UserCreateDto(BaseModel):
     login: str
     password: str
     name: str
     surname: str
     email: EmailStr
+    role: RoleDto
+
+class UserLoginDto(BaseModel):
+    login: str
+    password: str
+
+class UserDto(BaseModel):
+    id: int
+    login: str
+    name: str
+    surname: str
+    email: EmailStr
+    role:RoleDto
