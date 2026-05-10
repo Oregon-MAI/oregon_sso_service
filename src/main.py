b597dafb9e90d5c5162a222f0bbac1d737b91df4
Leonid Gurianov <leonid.11521@gmail.com>
2026-05-10 23:26:09 +0300

    metrics

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import structlog
 from fastapi import FastAPI
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.api.routers.auth_router import router as auth_router
 from src.api.routers.role_router import router as role_router
@@ -10,14 +11,17 @@ from src.log import init_logger
 from src.trace import init
 
 init_logger()
-
 init(SERVICE_NAME, JAEGER_ENDPOINT)
 
-app = FastAPI()
-
-logger = structlog.get_logger("sso")
+app = FastAPI(title="SSO Service")
 
 FastAPIInstrumentor.instrument_app(app)
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app)
+instrumentator.expose(app, endpoint="/metrics")
+
+logger = structlog.get_logger("sso")
 
 app.include_router(auth_router)
 app.include_router(user_router)
